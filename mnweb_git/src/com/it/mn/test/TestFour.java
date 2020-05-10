9fa66820af3ffe3854bff4a9a99fc5b6d2a6e032
zju_ljc.yyj <zju_ljc@126.com>
2020-05-10 16:56:20 +0800

    testFour 修改一行代码

--- mnweb_git/src/com/it/mn/test/TestFour.java
+++ mnweb_git/src/com/it/mn/test/TestFour.java
@@ -2,7 +2,7 @@ package com.it.mn.test;
 
 public class TestTwo {
 	public static void main(String[] args) {
-		System.out.println("hello world! 改为 hello hello !");
+		System.out.println("hello world! 改为 hello hello ! three edit");
 		System.out.println("hello two");
 	}
 
