7538123240cc697b61fbd696810dde1622e1ad27
zju_ljc.yyj <zju_ljc@126.com>
2020-05-10 17:10:44 +0800

    testFour 增加一行

--- mnweb_git/src/com/it/mn/test/TestFour.java
+++ mnweb_git/src/com/it/mn/test/TestFour.java
@@ -4,6 +4,7 @@ public class TestFour {
 	public static void main(String[] args) {
 		System.out.println("hello world! 改为 hello hello ! three edit");
 		System.out.println("hello two");
+		System.out.println("insert into");
 	}
 
 }
