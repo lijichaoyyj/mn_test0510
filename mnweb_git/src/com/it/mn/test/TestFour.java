fe12489260443a02c6db242dcde3a0a760a29ae3
zju_ljc.yyj <zju_ljc@126.com>
2020-05-10 17:29:50 +0800

    edit testFour

--- mnweb_git/src/com/it/mn/test/TestFour.java
+++ mnweb_git/src/com/it/mn/test/TestFour.java
@@ -4,7 +4,8 @@ public class TestFour {
 	public static void main(String[] args) {
 		System.out.println("hello world! 改为 hello hello ! three edit");
 		System.out.println("hello two");
-		System.out.println("insert");
+		System.out.println("insert p2");
+		System.out.println("this p2");
 	}
 
 }
